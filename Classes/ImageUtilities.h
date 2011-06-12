5e43f567ace0983be3b98f4b1c5defaba655aa95
pol <devnull@localhost>
2011-06-12 12:32:57 -0700

    Fixed some build failures

--- Classes/ImageUtilities.h
+++ Classes/ImageUtilities.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <TargetConditionals.h>
+#import <Foundation/Foundation.h>
 #if TARGET_OS_IPHONE
 #import <CoreGraphics/CoreGraphics.h>
 #else
